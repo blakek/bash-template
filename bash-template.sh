0d65ee6bd390b10604fb1be00a87b994c19c14b4
Blake Knight <oss.ideas@gmail.com>
2017-11-20 08:10:39 -0600

    :pencil: add initial examples and descriptions

--- bash-template.sh
+++ bash-template.sh
@@ -11,11 +11,19 @@ set -eu -o pipefail
 __dirname="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 __filename="$(basename "${BASH_SOURCE[0]}")"
 
+cleanup() {
+	# If there is any cleanup to do on an exit, do it here.
+	sshPID="$1"
+	kill sshPID
+}
+
 exampleFn() {
 	# Arguments are declared like this:
-	declare name="$1"
+	local name="$1"
+	local anotherArg="$2"
 	
-	# Always use quotes unless you have a reason not to.  Prefer enclosing variables in curly braces.
+	# Always use quotes unless you have a reason not to.  Prefer enclosing
+	# variables in curly braces.
 	echo "Hello, ${name}!"
 }
 
@@ -30,6 +38,18 @@ variadicFn() {
 main() {
 	# Use a "main" function that everything is passed to
 	exampleFn 'World'
+	
+	# Example: start process in background and kill it if this is killed
+	ssh user@example.com &
+	sshPID=$!
+	trap "cleanup ${sshPID}" EXIT
+	
+	# If your program is large and broken up well, you can opt to have the
+	# cleanup in the same location as your logic.  However, this can get messy
+	# quickly, so use wisely.
+	# Example: remove a temporary file if killed.  Note the semicolon is needed.
+	tempFile="$(mktemp -p /tmp)"
+	trap "{ rm -f ${tempFile}; }" EXIT
 }
 
-main
+main "$@"
