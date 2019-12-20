7ce27b70d91181477cca0b31ac1dea2cfff295b7
Blake Knight <bknight@gsandf.com>
2019-12-20 09:53:34 -0600

    :pencil: add more comments, make globals read-only, show variants of variadic function

--- bash-template.sh
+++ bash-template.sh
@@ -2,53 +2,69 @@
 set -eu -o pipefail
 
 ##
-# Description: Explain what the script is for
+# Description: Explain what the script is for. This is helpful if the script is
+# discovered outside a repository.
 # Date: 2017-01-31
 # Written by:
 #   - Blake Knight <oss.ideas@gmail.com> (http://blakek.me/)
 ##
 
-__dirname="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-__filename="$(basename "${BASH_SOURCE[0]}")"
+# The directory of the currently running file
+declare -r __dirname="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+# The filename of the currently running file
+declare -r __filename="$(basename "${BASH_SOURCE[0]}")"
 
 cleanup() {
-	# If there is any cleanup to do on an exit, do it here.
 	sshPID="$1"
 	kill sshPID
 }
 
 exampleFn() {
-	# Arguments are declared like this:
-	local name="$1"
-	local anotherArg="$2"
-	
-	# Always use quotes unless you have a reason not to.  Prefer enclosing
-	# variables in curly braces.
-	echo "Hello, ${name}!"
+	# When naming arguments, you should declare local variables. This prevents
+	# variables from being accidentally overwritten.
+	local greeting="$1"
+	local user="${2:-World}"
+
+	# Wrap variables in quotes to prevent globbing and word splitting (unless
+	# you have a reason not to). Prefer enclosing variables in curly braces when
+	# the variable is not used alone.
+	echo "${greeting}, ${user}!"
 }
 
 variadicFn() {
-	local arg1="$1"; shift
-	local arg2="$1"; shift
-	local rest="$@"
+	local arg1="$1"
+	shift
+	local arg2="$1"
+	shift
 
-	# ...
+	# To get the remaining arguments as an array, make sure to save the values
+	# as an array using `$@`.
+	local restArray=("$@")
+	# To get the remaining arguments as a string, use `$*`
+	local restString="$*"
+
+	echo "${arg1} ${arg2}"
+	echo "${restArray[*]}"
+	echo "$restString"
 }
 
+# Use a "main" function that everything is passed to
 main() {
-	# Use a "main" function that everything is passed to
-	exampleFn 'World'
-	
+	exampleFn 'Hello'
+
+	variadicFn This is an example with many arguments
+
 	# Example: start process in background and kill it if this is killed
 	ssh user@example.com &
 	sshPID=$!
 	trap "cleanup ${sshPID}" EXIT
-	
+
 	# If your program is large and broken up well, you can opt to have the
 	# cleanup in the same location as your logic.  However, this can get messy
 	# quickly, so use wisely.
-	# Example: remove a temporary file if killed.  Note the semicolon is needed.
-	tempFile="$(mktemp -p /tmp)"
+	# Example: remove a temporary file if killed.  Note, the semicolon is
+	# necessary.
+	tempFile="$(mktemp)"
 	trap "{ rm -f ${tempFile}; }" EXIT
 }
 
