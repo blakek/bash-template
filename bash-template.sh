febb79ad7581e79fba28aacd2275530417e1e002
Blake Knight <oss.ideas@gmail.com>
2021-07-12 22:20:20 -0500

    :sparkles: add updated `globals`, `showUsage` example, and parsing arguments

--- bash-template.sh
+++ bash-template.sh
@@ -2,70 +2,66 @@
 set -eu -o pipefail
 
 ##
-# Description: Explain what the script is for. This is helpful if the script is
-# discovered outside a repository.
-# Date: 2017-01-31
-# Written by:
-#   - Blake Knight <oss.ideas@gmail.com> (http://blakek.me/)
+# Write a brief description of the script and why it was created. This can be
+# helpful if the script is discovered outside a repository.
 ##
 
-# The directory of the currently running file
-declare -r __dirname="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-# The filename of the currently running file
-declare -r __filename="$(basename "${BASH_SOURCE[0]}")"
+declare -Ar globals=(
+	# The directory of the currently running file
+	['cwd']="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+	# The filename of the currently running file
+	['filename']="$(basename "${BASH_SOURCE[0]}")"
+	# The script's version
+	['version']="1.0.0"
+)
 
-cleanup() {
-	sshPID="$1"
-	kill sshPID
+showUsage() {
+	cat <<-END
+		Echos a list of given files
+		Usage:
+		    ${globals['filename']} [options] <file ...>
+		Options:
+		    -h, --help     output usage information and exit
+		    -V, --version  output the version number and exit
+		Positional arguments:
+		    files     example list of files for positional argument
+	END
 }
 
-exampleFn() {
-	# When naming arguments, you should declare local variables. This prevents
-	# variables from being accidentally overwritten.
-	local greeting="$1"
-	local user="${2:-World}"
+##
+# Use an entry function that everything is passed to. It's often renamed to
+# match the filename of the script.
+##
+templateExample() {
+	local -a files
 
-	# Wrap variables in quotes to prevent globbing and word splitting (unless
-	# you have a reason not to). Prefer enclosing variables in curly braces when
-	# the variable is not used alone.
-	echo "${greeting}, ${user}!"
+	# Parse arguments
+	for arg in "$@"; do
+		case "$arg" in
+			-h | --help | help)
+				showUsage
+				exit
+				;;
+			-V | --version)
+				echo "${globals['version']}"
+				exit
+				;;
+			-*)
+				printf "Error: unrecognized argument '%s'\n" "$arg" >&2
+				exit 1
+				;;
+			*) files+=("$arg") ;;
+		esac
+	done
+
+	for file in "${files[@]}"; do
+		echo "  - ${file}"
+	done
 }
 
-variadicFn() {
-	local arg1="$1"
-	shift
-	local arg2="$1"
-	shift
+if [[ ${BASH_SOURCE[0]} == "$0" ]]; then
+	templateExample "$@"
+	exit $?
+fi
 
-	# To get the remaining arguments as an array, make sure to save the values
-	# as an array using `$@`.
-	local restArray=("$@")
-	# To get the remaining arguments as a string, use `$*`
-	local restString="$*"
-
-	echo "${arg1} ${arg2}"
-	echo "${restArray[*]}"
-	echo "$restString"
-}
-
-# Use a "main" function that everything is passed to
-main() {
-	exampleFn 'Hello'
-
-	variadicFn This is an example with many arguments
-
-	# Example: start process in background and kill it if this is killed
-	ssh user@example.com &
-	sshPID=$!
-	trap "cleanup ${sshPID}" EXIT
-
-	# If your program is large and broken up well, you can opt to have the
-	# cleanup in the same location as your logic.  However, this can get messy
-	# quickly, so use wisely.
-	# Example: remove a temporary file if killed.  Note, the semicolon is
-	# necessary.
-	tempFile="$(mktemp)"
-	trap "{ rm -f ${tempFile}; }" EXIT
-}
-
-main "$@"
+export -f templateExample
